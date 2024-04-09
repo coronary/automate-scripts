4aafd3d44a3803ccc937021870cfb83bac279b3f
coronary <veradekalb@gmail.com>
2024-04-09 16:30:22 -0500

    timer: add minutes and seconds handler

--- timer.sh
+++ timer.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
 
 # TODO: handle adding hours
-#
-if [[ $# -gt 2 ]]; then
+
+function quitter {
 	echo ""
 	echo "incorrect number of arguments"
 	echo "this script requires between 0 and 2 inclusive arguments"
 	echo "use --help if you need details of what are valid inputs"
 	exit 1
+}
+
+seconds=0
+
+function add_minutes {
+	seconds=$(($seconds + $1*60))
+}
+
+function add_seconds {
+	seconds=$(($seconds + $1))
+}
+
+if [[ $# -gt 2 ]];then
+	# incorrect number of arguments
+	quitter
 fi
 
-seconds=$((5*60))
+if [[ $# -eq 1 ]];then
+	# only minutes added
+	add_minutes $1
+fi
 
-# if [[ $# -eq 1]];then
-# 	# only minutes added
-# fi
-#
-# if [[ $# -eq 2]];then
-# 	# no arguments so use default time
-# 	echo
-# fi
+if [[ $# -eq 2 ]];then
+	# minutes and seconds passed
+	add_minutes $1
+	add_seconds $2
+	echo
+fi
 
-osascript -e "delay $seconds" -e 'display notification "Please take a break" with title "TIME IS UP" sound name "Purr.aiff"' 2>/dev/null & disown 
+echo "timer set for $seconds seconds"
+osascript -e "delay $seconds" -e 'display notification "Please take a break" with title "TIME IS UP" sound name "Sosumi.aiff"' 2>/dev/null & disown 
